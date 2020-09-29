d37fd4efe9010cf6e17f50b7e29659784af82f3a
Sal Tijerina <r.sal.tijerina@gmail.com>
2020-09-29 17:03:30 -0500

    Update from develop

--- rootfs/startup.sh
+++ rootfs/startup.sh
@@ -13,34 +13,28 @@ if [ -n "$X11VNC_ARGS" ]; then
 fi
 
 if [ -n "$OPENBOX_ARGS" ]; then
-    sed -i "s#^command=/usr/bin/openbox.*#& ${OPENBOX_ARGS}#" /etc/supervisor/conf.d/supervisord.conf
+    sed -i "s#^command=/usr/bin/openbox\$#& ${OPENBOX_ARGS}#" /etc/supervisor/conf.d/supervisord.conf
 fi
 
 if [ -n "$RESOLUTION" ]; then
     sed -i "s/1024x768/$RESOLUTION/" /usr/local/bin/xvfb.sh
 fi
 
-# USER=${USER:-root}
-# HOME=/root
-# if [ "$USER" != "root" ]; then
-#     echo "* enable custom user: $USER"
-#     useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
-#     if [ -z "$PASSWORD" ]; then
-#         echo "  set default password to \"ubuntu\""
-#         PASSWORD=ubuntu
-#     fi
-#     HOME=/home/$USER
-#     echo "$USER:$PASSWORD" | chpasswd
-#     cp -r /root/{.gtkrc-2.0,.asoundrc} ${HOME}
-#     [ -d "/dev/snd" ] && chgrp -R adm /dev/snd
-# fi
-PASSWORD=designsafeubuntu1234!!
-USER="ubuntu"
-echo "$USER:$PASSWORD" | chpasswd
-HOME=/home/$USER
-cp -r /root/{.gtkrc-2.0,.asoundrc} ${HOME}
-[ -d "/dev/snd" ] && chgrp -R adm /dev/snd
-chown -R $USER:$USER $HOME/.[^.]*
+USER=${USER:-root}
+HOME=/root
+if [ "$USER" != "root" ]; then
+    echo "* enable custom user: $USER"
+    useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
+    if [ -z "$PASSWORD" ]; then
+        echo "  set default password to \"ubuntu\""
+        PASSWORD=ubuntu
+    fi
+    HOME=/home/$USER
+    echo "$USER:$PASSWORD" | chpasswd
+    cp -r /root/{.config,.gtkrc-2.0,.asoundrc} ${HOME}
+    chown -R $USER:$USER ${HOME}
+    [ -d "/dev/snd" ] && chgrp -R adm /dev/snd
+fi
 sed -i -e "s|%USER%|$USER|" -e "s|%HOME%|$HOME|" /etc/supervisor/conf.d/supervisord.conf
 
 # home folder
